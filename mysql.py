b7a82ca5370e225893856144e304e6d9263be351
sebastianconsuegra95 <sebastian951810@gmail.com>
2018-03-14 14:25:06 -0500

    ok

--- mysql.py
+++ mysql.py
@@ -17,7 +17,7 @@ def main():
     insertar = (
         "INSERT INTO syrus" "(latitud, longitud, hora)" "VALUES (%s, %s, %s)")
 
-    conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    conn = socket.socket(socket.AF_INT, socket.SOCK_DGRAM)
     conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     HOST = "172.31.90.245"
     conn.bind((HOST, 10701))

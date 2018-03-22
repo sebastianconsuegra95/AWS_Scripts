76689466962b45f40db8bd04afc216ee10891b11
sebastianconsuegra95 <sebastian951810@gmail.com>
2018-03-21 19:00:19 -0500

    ok

--- get2.php
+++ get2.php
@@ -2,16 +2,16 @@
 // Conectando, seleccionando la base de datos
 $conn = new mysqli("mydbdiseno2.crn0fxtqoene.us-east-1.rds.amazonaws.com", "root", "123456789", "dbsyrus"); // conecta al servidor con user, contraseña
 
-
+$range_1=$_COOKIE['range_1'];
+$range_2=$_COOKIE['range_2'];
 // Realizar una consulta MySQL
-$query = "SELECT * FROM syrus WHERE timems< $range_1 AND timems> $range_2  ORDER BY timems DESC"; // ultimo valor de la tabla llamada datos
+$query = "SELECT * FROM syrus WHERE ID> $range_1 AND ID< $range_2"; // ultimo valor de la tabla llamada datos
 $resultado = mysqli_query($conn, $query) or die("Consulta fallida: " . mysqli_error()); // guardo en resultado lo que saqué de query
-
-$fila = mysqli_fetch_row($resultado); // guardo en un array lo que está en resultado, como string
-
-$var = $fila[1]."\n".$fila[2]."\n".$fila[3]."\n";
-
- echo $var;
+$windows = mysqli_fetch_all($resultado); // guardo en un array lo que está en resultado, como string
+$row_lat=array_column($windows,1); 
+$row_lon=array_column($windows,2);
+$var = array_merge($row_lat,$row_lon);
+echo implode(" ",$var);
    mysqli_close($conn);
 
 ?>
